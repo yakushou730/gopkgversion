aa1c57787cc99625e02cd3be2dff9026510ace9b
shou_tseng <shou_tseng@trendmicro.com>
2023-02-13 13:52:26 +0800

    v2.2.0

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/yakushou730/gopkgversion/v3
+module github.com/yakushou730/gopkgversion/v2
 
 go 1.20
--- print.go
+++ print.go
@@ -3,7 +3,7 @@ package gopkgversion
 import "fmt"
 
 const (
-	VERSION = "2.1.0"
+	VERSION = "2.2.0"
 )
 
 func Print() {
